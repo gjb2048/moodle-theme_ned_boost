bee1d529c34b3f808b66e1f1bef562f32d24efab
Gareth J Barnard <gjbarnard@gmail.com>
2017-12-23 12:53:13 +0000

    Initial start of child theme.

---
+++ classes/toolbox.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    theme_ned_boost
+ * @subpackage NED
+ * @copyright  NED {@link http://ned.ca}
+ * @author     NED {@link http://ned.ca}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @developer  G J Barnard - {@link http://about.me/gjbarnard} and
+ *                           {@link http://moodle.org/user/profile.php?id=442195}
+ */
+
+namespace theme_ned_boost;
+
+defined('MOODLE_INTERNAL') || die();
+
+class toolbox {
+
+    protected static $instance;
+    protected $boostparent;
+
+    private function __construct() {
+    }
+
+    public static function get_instance() {
+        if (!is_object(self::$instance)) {
+            self::$instance = new self();
+            self::$instance->boostparent = \theme_config::load('boost');
+        }
+        return self::$instance;
+    }
+
+    public function get_main_scss_content($theme) {
+        global $CFG;
+        require_once($CFG->dirroot.'/theme/boost/lib.php');
+
+        $scss = theme_boost_get_main_scss_content($this->boostparent);
+
+        return $scss;
+    }
+}
---
+++ config.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    theme_ned_boost
+ * @subpackage NED
+ * @copyright  NED {@link http://ned.ca}
+ * @author     NED {@link http://ned.ca}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @developer  G J Barnard - {@link http://about.me/gjbarnard} and
+ *                           {@link http://moodle.org/user/profile.php?id=442195}
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Call the theme lib file.
+require_once($CFG->dirroot.'/theme/ned_boost/lib.php');
+
+$THEME->name = 'ned_boost';
+
+$THEME->parents = ['boost'];
+
+$THEME->sheets = [''];
+$THEME->editor_sheets = [''];
+
+$THEME->scss = function($theme) {
+    return theme_ned_boost_get_main_scss_content($theme);
+};
+
+$THEME->supportscssoptimisation = false;
+
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+
+$THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
+
+$THEME->enable_dock = false;
+$THEME->yuicssmodules = array();
+$THEME->requiredblocks = '';
